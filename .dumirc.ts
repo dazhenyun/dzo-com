236dd8660667784ed7a3902b95174364758e4fee
jws132 <jiangwenshu1990@126.com>
2024-03-04 16:20:34 +0800

    fix: add

--- .dumirc.ts
+++ .dumirc.ts
@@ -15,31 +15,21 @@ export default defineConfig({
     exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
     mode: 'site',
     mfsu: {},
-    nav: {
-      'zh-CN': [
-        {
-          title: '文档',
-          link: '/docs',
-        },
-        {
-          title: '组件',
-          link: '/components',
-        },
-        {
-          title: '工具类',
-          link: 'http://10.50.101.20:8091',
-        },
-      ],
-    },
-    extraBabelPlugins: [
-      [
-        'babel-plugin-import',
-        {
-          libraryName: 'antd',
-          libraryDirectory: 'es',
-          style: true,
-        },
-      ],
-    ],
+    // nav: {
+    //   'zh-CN': [
+    //     {
+    //       title: '文档',
+    //       link: '/docs',
+    //     },
+    //     {
+    //       title: '组件',
+    //       link: '/components',
+    //     },
+    //     {
+    //       title: '工具类',
+    //       link: 'http://10.50.101.20:8091',
+    //     },
+    //   ],
+    // },
   },
 });
--- .fatherrc.ts
+++ .fatherrc.ts
@@ -5,14 +5,4 @@ export default {
   //   generateScopedName: '@dzo/com_[name]__[local]___[hash:base64:5]',
   // },
   extractCSS: true,
-  extraBabelPlugins: [
-    [
-      'babel-plugin-import',
-      {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: true,
-      },
-    ],
-  ],
 };
